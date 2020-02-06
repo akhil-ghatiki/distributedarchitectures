347eb0fc1a8bd4e31b09126abf9bfc25aa4d12dc
Akhil Ghatiki <akhil_ghatiki@gap.com>
2020-02-07 01:22:07 +0530

    Implement zookeeper client

---
+++ src/main/scala/org/dist/mysimplekafka/MyZookeeperClient.scala
@@ -0,0 +1,45 @@
+package org.dist.mysimplekafka
+
+import org.I0Itec.zkclient.ZkClient
+import org.I0Itec.zkclient.exception.ZkNoNodeException
+import org.dist.kvstore.JsonSerDes
+import org.dist.queue.utils.ZkUtils
+
+import scala.jdk.CollectionConverters._
+
+class MyZookeeperClient(zkClient: ZkClient) {
+  val BrokerIdsPath = "/brokers/ids"
+
+  def registerBroker(broker: ZkUtils.Broker)= {
+    val brokerData = JsonSerDes.serialize(broker)
+    val brokerPath = getBrokerPath(broker.id)
+    createEphemeralPath(zkClient, brokerPath, brokerData)
+
+  }
+
+  def getAllBrokerIds(): Set[Int] = {
+    zkClient.getChildren(BrokerIdsPath).asScala.map(_.toInt).toSet
+  }
+
+  private def getBrokerPath(id: Int) = {
+    BrokerIdsPath + "/" + id
+  }
+
+  def createEphemeralPath(client: ZkClient, path: String, data: String): Unit = {
+    try {
+      client.createEphemeral(path, data)
+    } catch {
+      case e: ZkNoNodeException => {
+        createParentPath(client, path)
+        client.createEphemeral(path, data)
+      }
+    }
+  }
+
+  private def createParentPath(client: ZkClient, path: String): Unit = {
+    val parentDir = path.substring(0, path.lastIndexOf('/'))
+    if (parentDir.length != 0)
+      client.createPersistent(parentDir, true)
+  }
+
+}
